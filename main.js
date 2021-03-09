658c94eaa069de35942f560ebb7f80403f921579
Ivan Ivanitskiy <iivanitskiy@mail.ru>
2021-03-09 12:00:37 +0300

    final

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(function() {
   function setColor() {
-    let red = $( "#red" ).slider( "value" ),
-      green = $( "#green" ).slider( "value" ),
-      blue = $( "#blue" ).slider( "value" );
-    $( "#background-id" ).css( "background-color", "#" + "rgb(" + red + ", " + green + ", " + blue + ")" );
-    $( "#text-id" ).css( "color", "#" + "rgb(" + red + ", " + green + ", " + blue + ")" );
+    let red = $("#red").slider("value"),
+      green = $("#green").slider("value"),
+      blue = $("#blue").slider("value");
+    $("#background-id").css("background-color", `rgb(${red}, ${green}, ${blue})`);
+    $("#text-id").css("color", `rgb(${red}, ${green}, ${blue})`);
   }
 
-  $( "#red, #green, #blue" ).slider({
+  $("#red, #green, #blue").slider({
     orientation: "horizontal",
     range: "min",
     max: 255,
@@ -16,12 +16,12 @@ $(function() {
     change: setColor
   });
 
-  $("#color" ).click(function() {
+  $("#color").click(function() {
     $(".background").attr('id', '');
     $(".text").attr('id', 'text-id');
   });
 
-  $("#backgroundColor" ).click(function() {
+  $("#backgroundColor").click(function() {
     $(".text").attr('id', '');  
     $(".background").attr('id', 'background-id');
   });
